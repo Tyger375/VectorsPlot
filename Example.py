2c55701eef0bdf20d11cdf760efc6856833f7deb
Tyger375 <edoardo.takanen@gmail.com>
2022-02-28 21:06:05 +0100

    Fixed some bugs and added something for plotting data

--- Example.py
+++ Example.py
@@ -1,20 +1,49 @@
-from VectorsPlot import *
+from VectorsPlot import Vector2D, Grafico
 
-vettore = Vector2D(15, 15)
 
-print(vettore + Vector2D(2, 3), vettore - Vector2D(2, 3))
-test = Grafico()
-test.set_size((Vector2D(0, 0), vettore))
-test.set_x_interval(1)
-test.set_y_interval(1)
-test.set_title("sos")
-primovettore = Vector2D(2, 4)
-secondovettore = Vector2D(3, 1)
-#test.punta_coda([primovettore, secondovettore])
-test.parallelogramma(primovettore, secondovettore)
-#test.add_arrow(primovettore)
-#test.add_arrow(vectorfrom=primovettore, vectorto=secondovettore)
-#test.add_arrow(primovettore + secondovettore)
-#test.add_arrow(Vector2D(2, 3))
-test.set_grid(True)
-test.save("test.png")
+def graficovettori():
+    vettore = Vector2D(15, 15)
+
+    print(vettore + Vector2D(2, 3), vettore - Vector2D(2, 3))
+    test = Grafico()
+    test.set_size((Vector2D(0, 0), vettore))
+    test.set_x_interval(1)
+    test.set_y_interval(1)
+    test.set_title("sos")
+    primovettore = Vector2D(2, 4)
+    secondovettore = Vector2D(3, 1)
+    #test.add_arrow(secondovettore)
+    #test.punta_coda([primovettore, secondovettore])
+    test.parallelogramma([primovettore, secondovettore, Vector2D(2, 3)])
+    print(primovettore + secondovettore + Vector2D(2, 3))
+    #test.add_arrow(primovettore)
+    #test.add_arrow(vectorfrom=primovettore, vectorto=secondovettore)
+    #test.add_arrow(primovettore + secondovettore)
+    #test.add_arrow(Vector2D(2, 3))
+    test.set_grid(True)
+    test.save("test.png")
+
+
+def grafico():
+    #Forza-allungamento
+    grafico = Grafico()
+    grafico.set_size((Vector2D(0, 0), Vector2D(2, 20)))
+    grafico.set_x_interval(0.25)
+    grafico.set_y_interval(1)
+    grafico.plot([2, 1], [2, 1], "points")
+    grafico.save()
+
+
+grafico()
+#ax.arrow(2.0, 4.0, 6.0, test = Grafico()4#.0,.save() head_width=0.5, head_length=0.7, fc='lightblue', ec='black')
+
+#plt.grid()
+
+#plt.xlim(0,10#)
+#plt.ylim(0#,10)
+
+#plt.title#('How to plot a #vector in matplotlib ?',fontsize=10)
+
+#plt.savefig('how_to_plot_#a_vector_in_matplotlib_fig1.png', bbox_inches='tight')
+#plt.show()
+#plt.close()
--- VectorsPlot.py
+++ VectorsPlot.py
@@ -18,10 +18,15 @@ class Vector2D():
       raise Exception("Invalid type")
     return Vector2D((self.x - other.x), (self.y - other.y))
     
-xticks=None, yticks=None):
-        self.ax.set_xticks(np.arange(size[0].x, (size[1].x + 1), xticks))
-      if yticks != None:
-        self.ax.set_yticks(np.arange(size[0].y, (size[1].y + 1), yticks))
+class Grafico:
+  def __init__(self, size=None, xticks=None, yticks=None):
+    fig, ax = plt.subplots()
+    self.fig = fig
+    self.ax = ax
+    if size:
+      self.ax.set_xticks(np.arange(size[0].x, (size[1].x + 1), xticks))
+    if yticks != None:
+      self.ax.set_yticks(np.arange(size[0].y, (size[1].y + 1), yticks))
 
   def set_title(self, title):
     self.ax.set_title(title)
@@ -45,4 +50,34 @@ xticks=None, yticks=None):
       self.ax.grid(color='black', ls = '-', lw = 0.25)
     else:
       self.ax.grid(False)
-
+  def save(self, nomefile="grafico.png"):
+    self.fig.savefig(nomefile)
+  def punta_coda(self, vectors):
+    sommavettori = Vector2D()
+    for vettore in vectors:
+      self.add_arrow(vettore, sommavettori)
+      sommavettori += vettore
+    self.add_arrow(sommavettori)
+  def parallelogramma(self, vectors):
+    primovettore = None
+    sommavettori = Vector2D()
+    for vettore in vectors:
+      print(vettore)
+      if not primovettore:
+        primovettore = vettore
+      else:
+        self.add_arrow(primovettore, vectorfrom=sommavettori)
+        self.add_arrow(vettore, vectorfrom=(sommavettori + primovettore))
+        self.add_arrow(primovettore, vectorfrom=(sommavettori + vettore), linestyle="dashed")
+        self.add_arrow(vettore, vectorfrom=sommavettori, linestyle="dashed")
+        self.add_arrow(primovettore + vettore, vectorfrom=sommavettori)
+        sommavettori = sommavettori + primovettore + vettore
+        primovettore = vettore
+        break
+  def plot(self, x, y, type="lines"):
+    if type == "lines":
+      self.ax.plot(x, y)
+    elif type == "points":
+      self.ax.scatter(x, y)
+    else:
+      print("invalid type")
